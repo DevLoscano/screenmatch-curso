bfe1f1286809a4f27f95a262e9b0c40cad414e64
Gabriel Loscano <loscanodev@gmail.com>
2024-12-17 19:00:37 -0300

    iniciando projeto spring

--- src/main/java/br/com/alura/screnmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screnmatch/model/DadosSerie.java
@@ -3,7 +3,7 @@ package br.com.alura.screnmatch.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(ignoreUnknown = true) // Ignorar dados não incluido, por deafult ignoreUnknown vem falso
+@JsonIgnoreProperties(ignoreUnknown = true) // Ignorar dados não incluido, por deafult ignoreUnknown vem falso teste
 public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
                          @JsonAlias("imdbRating") String avaliacao) {
